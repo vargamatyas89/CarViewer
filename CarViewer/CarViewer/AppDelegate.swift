1429651987c9be3dc377836b223d1617ef078690
Varga, Matyas <matyas.varga@sap.com>
2017-10-19 20:53:20 +0200

    application doesn't use user location

--- CarViewer/CarViewer/AppDelegate.swift
+++ CarViewer/CarViewer/AppDelegate.swift
@@ -21,8 +21,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
         let navigationController = splitViewController.viewControllers[splitViewController.viewControllers.count-1] as! UINavigationController
         navigationController.topViewController!.navigationItem.leftBarButtonItem = splitViewController.displayModeButtonItem
         splitViewController.delegate = self
-        
-        self.requestLocationAccess()
 
         return true
     }
